15299c1c415e34f97dd4d03548eebd4aeaafbd1d
antoniorusso2 <russoantonio303@outlook.it>
2025-01-08 16:36:36 +0100

    update git ignore

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //express basic vars
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //middlewares
 const notFound = require('./middlewares/notFound');
