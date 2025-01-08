c72603ecb07cf0ca091175d180be8caf93b294ca
antoniorusso2 <russoantonio303@outlook.it>
2025-01-08 17:15:47 +0100

    implement filter with title for index function

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,10 +1,24 @@
 const connection = require('../data/db');
 
 function index(req, res) {
-  const sql = `SELECT * FROM movies_db.movies;`;
+  let sql = `SELECT * FROM movies_db.movies`;
+
+  const titleQuery = req.query.title.trim();
+
+  console.log(titleQuery);
+
+  //filtro per titolo o parte di titolo
+  if (titleQuery) {
+    sql = `SELECT * FROM movies_db.movies WHERE movies.title LIKE '%${titleQuery}%'`;
+  }
 
   connection.query(sql, (err, movies) => {
     if (err) return res.status(500).json({ message: err.message });
+    if (movies.length === 0)
+      return res.status(404).json({
+        error: 'not found',
+        message: 'movie not found',
+      });
     res.json(movies);
   });
 }
