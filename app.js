00721783c3efe3349b0f27b769bd8cc52e2f206d
antoniorusso2 <russoantonio303@outlook.it>
2025-01-08 15:52:02 +0100

    comments + showing public files + install mysql2 dependencies

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 console.log('app');
 
+//express basic vars
 const express = require('express');
 const app = express();
 const port = 3000;
 
+//middlewares
 const notFound = require('./middlewares/notFound');
 const errorsHandler = require('./middlewares/errorsHandler');
 
 app.get('/', (req, res) => {
   res.send('server running');
 });
+
+//middlewares use
+app.use(express.static('public/imgs')); //public files in public/img
 
 app.use('/', errorsHandler);
 app.use('/', notFound);
