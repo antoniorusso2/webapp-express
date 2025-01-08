bb1354a756d15763583164fd305ea145b6447844
antoniorusso2 <russoantonio303@outlook.it>
2025-01-08 14:35:20 +0100

    first commit: add gitignore, install express, add img folder

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log('app');
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('server running');
+});
+
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
