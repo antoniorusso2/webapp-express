8dcbe538222772c143159cbdd3f028718c8ad240
antoniorusso2 <russoantonio303@outlook.it>
2025-01-08 17:57:50 +0100

    fix show not working and index reverted to previous state because last feature not working as intended

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -15,21 +15,34 @@ function index(req, res) {
 
   let sql = `SELECT * FROM movies_db.movies`;
 
-  const titleQuery = req.query.title.trim();
+  const titleQuery = req.query.title;
 
   //filtro per titolo o parte di titolo
   if (titleQuery) {
+    //concatenazione query sql in caso il campo esista
     sql += ` WHERE movies.title LIKE ?`;
-  } else if (titleQuery.length < 1) {
-    return res.status(400).json({ message: 'bad request' });
+    connection.query(sql, `%${titleQuery.trim()}%`, callback);
+    return;
   }
-
-  connection.query(sql, `%${titleQuery}%`, callback);
+  //ricerca di tutti i post normalmente
+  connection.query(sql, callback);
 }
 
 function show(req, res) {
   //recupero parametro dinamico da url
   const id = req.params.id;
+
+  const callback = (error, results) => {
+    if (error) return res.status(500).json({ massage: err.message });
+
+    if (results.length === 0)
+      return res.status(404).json({
+        error: 'not found',
+        message: 'movie not found',
+      });
+
+    res.json(results);
+  };
 
   const sql = `SELECT * FROM movies_db.movies WHERE movies.id = ?`;
 
