f18c27e6185e244050794e7928ae9fc2d9767aa0
antoniorusso2 <russoantonio303@outlook.it>
2025-01-08 18:17:14 +0100

    implement reviews when selecting a single film by id

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -35,21 +35,44 @@ function show(req, res) {
   //recupero parametro dinamico da url
   const id = req.params.id;
 
-  const callback = (error, results) => {
-    if (error) return res.status(500).json({ massage: err.message });
+  // const callback = (error, results) => {
+  //   if (error) return res.status(500).json({ massage: err.message });
 
-    if (results.length === 0)
-      return res.status(404).json({
-        error: 'not found',
-        message: 'movie not found',
-      });
+  //   if (results.length === 0)
+  //     return res.status(404).json({
+  //       error: 'not found',
+  //       message: 'movie not found',
+  //     });
 
-    res.json(results);
-  };
+  //   res.json(results);
+
+  // };
 
   const sql = `SELECT * FROM movies_db.movies WHERE movies.id = ?`;
 
-  connection.query(sql, [id], callback);
+  connection.query(sql, [id], (err, results) => {
+    if (err) return res.status(500).json({ massage: err.message });
+
+    if (results.length === 0) {
+      return res.status(400).json({
+        error: 'not found',
+        message: 'movie not found',
+      });
+    }
+
+    const movie = results[0];
+
+    const reviewSQL = `SELECT * FROM movies_db.reviews WHERE movie_id = ?`;
+
+    connection.query(reviewSQL, [id], (err, results) => {
+      if (err) return res.status(500).json({ message: err.message });
+
+      movie.reviews = results;
+      res.json(movie);
+    });
+  });
+
+  connection;
 
   //show per le recensioni
 }
